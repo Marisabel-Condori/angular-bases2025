46b3d3dea852e2619b0e419ebbf7153ca4a20f8d
Mari <mari.17ymy@gmail.com>
2025-10-26 16:53:42 -0400

    signals

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,14 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
-    template: `
-    <h1>Counter: {{counter}}</h1>
-    <button (click)="increaceBy(1)">+1</button>
-    `
+    selector: 'counter-page',
+    templateUrl: './counter-page.component.html',
 })
 export class CounterPageComponent{
     counter = 10
+    counterSignal = signal(10)
+
     increaceBy(value: number){
         this.counter += value
+        this.counterSignal.update( current => current + value)
+    }
+
+    reset(){
+        this.counter = 10
+        this.counterSignal.set(0)
     }
 }
