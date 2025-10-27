d247f06a07a687ebcef5fdff84131d40f7c158fc
Mari <mari.17ymy@gmail.com>
2025-10-27 00:36:09 -0400

    condicionales

--- src/app/pages/dragonball/dragonball.ts
+++ src/app/pages/dragonball/dragonball.ts
@@ -17,6 +17,7 @@ export class Dragonball {
     { id: 1, name: 'Goku', power: 9001 },
     { id: 1, name: 'Vegeta', power: 8000 },
     { id: 1, name: 'Picolo', power: 3000 },
+    { id: 1, name: 'Yamcha', power: 500 },
   ]);
 
   //para la forma ngClass
