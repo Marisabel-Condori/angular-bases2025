6ef71ad99d8fc31fe6d9b0c015a22fde78aa8fb8
Marisabel Condori Cano <mccondori@entel.bo>
2025-10-28 16:17:03 -0400

    funcionamiento de rutas, en la nube

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,9 +1,18 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage  = (): Character[] => {
+  const characters  = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : []
+}
 
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
   characters = signal<Character[]>([{ id: 1, name: 'Goku', power: 9001 }]);
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
+  })
 
   addCharacter(character: Character) {
     this.characters.update((list) => [...list, character]);
